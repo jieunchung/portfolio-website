1e91d1f5f0984f2b316373dc2ebb9581cde3ab14
jieunchung <106100696+jieunchung@users.noreply.github.com>
2022-10-29 12:13:28 +0200

    Clean code

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,6 +2,36 @@ import React from "react";
 import { MdOpenInNew } from "react-icons/md";
 
 const Work = (props) => {
+  const Works = [
+    {
+      title: "Tenzies",
+      url: "https://ephemeral-lolly-e84e7e.netlify.app/",
+      description:
+        "A fast-paced dice game, where you need to make all 10 dice the same value.",
+      technologies: "React CSS",
+    },
+    {
+      title: "Weather?",
+      url: "https://mellifluous-cajeta-05ad88.netlify.app/",
+      description:
+        "A minimal weather app that provides information regarding weather conditions around the world.",
+      technologies: "React Bootstrap API",
+    },
+    {
+      title: "/Find",
+      url: "https://superb-crostata-920b96.netlify.app/",
+      description:
+        "An English dictionary app with definitions, synonyms, pronunciations, sentences, and pictures.",
+      technologies: "React Bootstrap API",
+    },
+    {
+      title: "Meme Generator",
+      url: "https://warm-flan-da771a.netlify.app/",
+      description:
+        "A web application that allows users to quickly and easily generate custom memes.",
+      technologies: "React API Tailwind",
+    },
+  ];
   return (
     <div
       name="work"
@@ -22,100 +52,35 @@ const Work = (props) => {
             </h2>
           </div>
         </div>
-        {/* container for items */}
+        {/* works */}
         <ul className="w-10/12 2xl:w-[80%] mx-auto border-[transparent] flex flex-col lg:flex-row lg:min-h-[30vh] justify-center gap-2 font-[Vollkorn] flex-wrap">
-          <li
-            className={`flex flex-col border-[#fbfbfd] flex-1 m-4 bg-[#fbfbfd] ${
-              props.darkMode && "bg-[#2a2a2a] border-[#2a2a2a]"
-            } min-h-[10rem]`}
-          >
-            <a
-              className="flex justify-end m-2 text-xl"
-              href="https://ephemeral-lolly-e84e7e.netlify.app/"
-              target="_blank"
-            >
-              <MdOpenInNew />
-            </a>
-            <h4 className="flex-1 text-center text-xl 2xl:text-2xl VollkornSC my-2">
-              tenzies
-            </h4>
-            <p className="flex-1 flex items-center justify-center text-[0.9rem] 2xl:text-[1rem] mx-4 lg:mx-1 text-center xl:text-center">
-              A fast-paced dice game, where you need to make all 10 dice the
-              same value.
-            </p>
-            <footer className="flex-1 flex items-end justify-center mx-4 text-[0.75rem] xl:text-[0.85rem] opacity-[75%] my-1">
-              React CSS
-            </footer>
-          </li>
-          <li
-            className={`flex flex-col border-[#fbfbfd] flex-1 m-4 bg-[#fbfbfd] ${
-              props.darkMode && "bg-[#2a2a2a] border-[#2a2a2a]"
-            } min-h-[10rem]`}
-          >
-            <a
-              className="flex justify-end m-2 text-xl"
-              href="https://mellifluous-cajeta-05ad88.netlify.app/"
-              target="_blank"
-            >
-              <MdOpenInNew />
-            </a>
-            <h4 className="flex-1 text-center text-xl 2xl:text-2xl VollkornSC my-2">
-              weather?
-            </h4>
-            <p className="flex-1 flex items-center justify-center text-[0.9rem] 2xl:text-[1rem] mx-4 lg:mx-1 text-center xl:text-center">
-              a minimal weather app that provides information regarding weather
-              conditions around the world.
-            </p>
-            <footer className="flex-1 flex items-end justify-center mx-4 text-[0.75rem] xl:text-[0.85rem] opacity-[75%] my-1">
-              React Bootstrap API
-            </footer>
-          </li>
-          <li
-            className={`flex flex-col border-[#fbfbfd] flex-1 m-4 bg-[#fbfbfd] ${
-              props.darkMode && "bg-[#2a2a2a] border-[#2a2a2a]"
-            } min-h-[10rem]`}
-          >
-            <a
-              className="flex justify-end m-2 text-xl"
-              href="https://superb-crostata-920b96.netlify.app/"
-              target="_blank"
-            >
-              <MdOpenInNew />
-            </a>
-            <h4 className="flex-1 text-center text-xl 2xl:text-2xl VollkornSC my-2">
-              /find
-            </h4>
-            <p className="flex-1 flex items-center justify-center text-[0.9rem] 2xl:text-[1rem] mx-4 lg:mx-1 text-center xl:text-center">
-              An English dictionary app with definitions, synonyms,
-              pronunciations, sentences, and pictures.
-            </p>
-            <footer className="flex-1 flex items-end justify-center mx-4 text-[0.75rem] xl:text-[0.85rem] opacity-[75%] my-1">
-              React Bootstrap API
-            </footer>
-          </li>
-          <li
-            className={`flex flex-col border-[#fbfbfd] flex-1 m-4 bg-[#fbfbfd] ${
-              props.darkMode && "bg-[#2a2a2a] border-[#2a2a2a]"
-            } min-h-[10rem]`}
-          >
-            <a
-              className="flex justify-end m-2 text-xl"
-              href="https://warm-flan-da771a.netlify.app/"
-              target="_blank"
-            >
-              <MdOpenInNew />
-            </a>
-            <h4 className="flex-1 text-center text-xl 2xl:text-2xl VollkornSC my-2">
-              meme generator
-            </h4>
-            <p className="flex-1 flex items-center justify-center text-[0.9rem] 2xl:text-[1rem] mx-4 lg:mx-1 text-center xl:text-center">
-              A web application that allows users to quickly and easily generate
-              custom memes.
-            </p>
-            <footer className="flex-1 flex items-end justify-center mx-4 text-[0.75rem] xl:text-[0.85rem] opacity-[75%] my-1">
-              React API Tailwind
-            </footer>
-          </li>
+          {Works.map((work, index) => {
+            return (
+              <li
+                key={index}
+                className={`flex flex-col border-[#fbfbfd] flex-1 m-4 bg-[#fbfbfd] ${
+                  props.darkMode && "bg-[#2a2a2a] border-[#2a2a2a]"
+                } min-h-[10rem]`}
+              >
+                <a
+                  className="flex justify-end m-4 text-xl"
+                  href={work.url}
+                  target="_blank"
+                >
+                  <MdOpenInNew />
+                </a>
+                <h4 className="flex-1 text-center text-xl 2xl:text-2xl VollkornSC my-2 lowercase">
+                  {work.title}
+                </h4>
+                <p className="flex-1 flex items-center justify-center text-[0.9rem] 2xl:text-[1rem] mx-4 lg:mx-1 text-center xl:text-center">
+                  {work.description}
+                </p>
+                <footer className="flex-1 flex items-end justify-center mx-4 text-[0.75rem] xl:text-[0.85rem] opacity-[75%] my-4">
+                  {work.technologies}
+                </footer>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
